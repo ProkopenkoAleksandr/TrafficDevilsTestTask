b1d4aa032df93d2c98832f5ce3bf381415297572
Aleksandr Prokopenko <aleksandr.prokopenko.dev@gmail.com>
2023-11-17 23:39:00 +0300

    Prepared project for work -Removed Storyboard -Removed extra files -Inialized main scene for next steps

--- TrafficDevilsTestTask/GameViewController.swift
+++ TrafficDevilsTestTask/GameViewController.swift
@@ -10,24 +10,20 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
+    
+    private var customView: SKView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+        customView = SKView(frame: self.view.frame)
+        
+        if customView != nil {
+            self.view.addSubview(customView!)
+            let scene = MainScene(size: (view?.bounds.size)!)
+            scene.scaleMode = .aspectFill
+
+            customView?.presentScene(scene)
         }
     }
 
---
+++ TrafficDevilsTestTask/MainScene.swift
@@ -0,0 +1,16 @@
+//
+//  MainScene.swift
+//  TrafficDevilsTestTask
+//
+//  Created by Prokopenko Aleksandr on 17.11.2023.
+//
+
+import UIKit
+import SpriteKit
+
+class MainScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        
+    }
+}
